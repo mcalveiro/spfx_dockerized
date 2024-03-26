4ff539e98f60d03f481a176043ee3024e6d8324a
Miguel A. Calveiro <mcalveiro@gmail.com>
2024-03-26 15:49:16 -0300

    [v.0.01]
    Versión starter.
    SPFx

---
+++ src/webparts/blank/components/IBlankProps.ts
@@ -0,0 +1,7 @@
+export interface IBlankProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/blank/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IBlankWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'BlankWebPartStrings' {
+  const strings: IBlankWebPartStrings;
+  export = strings;
+}
